9b83a8d0292a714ef61689b945147d64a33bd8bc
zl1509015035 <1509015035@qq.com>
2021-02-20 20:20:25 +0800

    BeanFactory

--- spring5_demo1/src/com/zhul/spring5/testdemo/TestSpring5.java
+++ spring5_demo1/src/com/zhul/spring5/testdemo/TestSpring5.java
@@ -2,6 +2,7 @@ package com.zhul.spring5.testdemo;
 
 import com.zhul.spring5.User;
 import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -11,6 +12,7 @@ public class TestSpring5 {
     public void testAdd(){
         //1.加载spring配置文件
         ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+//        BeanFactory context = new ClassPathXmlApplicationContext("bean1.xml");
         //2.获取配置创建的对象
         User user = context.getBean("user", User.class);
         System.out.println(user);
