7890635908e290b1b383d3b57eac4997cb05d06c
zl1509015035 <1509015035@qq.com>
2021-03-01 22:29:36 +0800

    spring_demo3

---
+++ spring5_demo4/src/com/zhul/spring5/UserDao.java
@@ -0,0 +1,8 @@
+package com.zhul.spring5;
+
+public interface UserDao {
+
+    public int add(int a,int b);
+
+    public String update(String id);
+}
---
+++ spring5_demo4/src/com/zhul/spring5/UserDaoImpl.java
@@ -0,0 +1,13 @@
+package com.zhul.spring5;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public String update(String id) {
+        return id;
+    }
+}
